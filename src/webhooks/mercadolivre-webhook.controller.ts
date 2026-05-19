e79ec5d7b390109cfe9c73955d5230de5645f186
CPT_Caverna <mateusdeangeli6@gmail.com>
2026-05-19 00:55:17 -0300

    Teste webhook

--- src/webhooks/mercadolivre-webhook.controller.ts
+++ src/webhooks/mercadolivre-webhook.controller.ts
@@ -1,22 +1,14 @@
-import { Body, Controller, HttpCode, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Req, UsePipes, ValidationPipe } from '@nestjs/common';
 import { MercadoLivreNotificationDto } from './dto/mercadolivre-notification.dto';
 import { MercadoLivreWebhookService } from './mercadolivre-webhook.service';
 
 @Controller('webhooks/mercadolivre')
 export class MercadoLivreWebhookController {
-  constructor(private readonly webhookService: MercadoLivreWebhookService) {}
-
   @Post()
   @HttpCode(200)
-  @UsePipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: false,
-      transform: true,
-    }),
-  )
-  receive(@Body() notification: MercadoLivreNotificationDto) {
-    this.webhookService.enqueueProcess(notification);
+  receive(@Body() body: any) {
+    console.log(body);
+
     return { ok: true };
   }
 }
